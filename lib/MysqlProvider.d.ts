4603d8b8a082aab213f8cf05a100ef5693bf9b1d
Daghdha <daghdha1.dev@gmail.com>
2022-12-17 14:35:18 +0100

    fix: folder project renamed

--- lib/MysqlProvider.d.ts
+++
@@ -1,3 +0,0 @@
-import { MysqlConfiguration } from './MysqlConfiguration';
-import * as mysql from 'mysql2/promise';
-export declare const MysqlProvider: (config: MysqlConfiguration) => Promise<mysql.Pool>;
