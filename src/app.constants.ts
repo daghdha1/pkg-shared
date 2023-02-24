5b88796a8dab3bba055479ea1151587d80d10c11
Daghdha <daghdha1.dev@gmail.com>
2023-02-24 13:34:24 +0100

    fix: add enum providers

--- src/app.constants.ts
+++ src/app.constants.ts
@@ -1,3 +1,11 @@
 export const appConstants = {
  appName: "tracking-lab",
 };
+
+export enum Provider {
+ MySQL = "MYSQL",
+ Knex = "KNEX",
+ Mongo = "MONGO",
+ Redis = "REDIS",
+ Kafka = "KAFKA",
+}
