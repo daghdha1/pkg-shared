8c4a4e670f9d21e5e7bf666575f0217f838a6147
Daghdha <daghdha1.dev@gmail.com>
2023-03-01 18:54:52 +0100

    fix: test of moduleInit

--- lib/kafka-custom-provider/kafka.module.d.ts
+++ lib/kafka-custom-provider/kafka.module.d.ts
@@ -1,2 +1,7 @@
-export declare class KafkaModule {
+import { OnModuleInit } from "@nestjs/common";
+import { ClientKafka } from "@nestjs/microservices";
+export declare class KafkaModule implements OnModuleInit {
+    private readonly kafkaClient;
+    constructor(kafkaClient: ClientKafka);
+    onModuleInit(): Promise<void>;
 }
