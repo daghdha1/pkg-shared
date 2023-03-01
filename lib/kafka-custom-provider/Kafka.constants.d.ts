f752bb4c90b846bef6dccd677f17d39afd29c953
Daghdha <daghdha1.dev@gmail.com>
2023-03-01 16:39:52 +0100

    fix: KafkaConstants added with lib again

--- lib/kafka-custom-provider/Kafka.constants.d.ts
+++ lib/kafka-custom-provider/Kafka.constants.d.ts
@@ -2,4 +2,8 @@ export declare const KafkaConstants: {
     kafkaProducerId: string;
     kafkaConsumerId: string;
     brokers: string[];
+    logLevel: number;
+    fromBeginning: boolean;
+    producerOnlyMode: boolean;
+    allowAutoTopicCreation: boolean;
 };
